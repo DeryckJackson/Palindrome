1cd23c6a67793f38c9ef4f1c645070c48c1e59b9
Tyson Lackey <lackeyt90@gmail.com>
2020-07-14 10:04:21 -0700

    greenlight reversed not equal to input test
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- Palindrome.Tests/PalindromeTests.cs
+++ Palindrome.Tests/PalindromeTests.cs
@@ -12,5 +12,12 @@ namespace Palindrome.Tests
       Palindrome testPalindrome = new Palindrome();
       Assert.AreEqual(true, testPalindrome.IsPalindrome("racecar"));
     }
+
+    [TestMethod]
+    public void IsPalindrome_InputStringReveresedNotEqualToInputString_False()
+    {
+      Palindrome testPalindrome = new Palindrome();
+      Assert.AreEqual(false, testPalindrome.IsPalindrome("spaceship"));
+    }
   }
 }
